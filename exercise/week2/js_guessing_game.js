d296817998081be9a7e405fbe9fbc8303f52e3d7
hungnm27 <hungnm27@fsoft.com.vn>
2017-04-08 10:01:19 +0700

    Update exercies html + lecture

--- exercise/week2/js_guessing_game.js
+++ exercise/week2/js_guessing_game.js
@@ -1,12 +1,9 @@
 var guessTarget;
-var guessTargetText;
 var finisihed;
 var colors = new Array("red","green","blue","orange","yellow",
                        "brown","violet","gray","black","white",
                        "purple","pink","golden","silver","cyan");
 var guessInputText;
-var guessInput;
-
 var guessCount = 0;
 
 function do_game() {
@@ -15,29 +12,31 @@ function do_game() {
 
   while(!finisihed)
   {
-    guessInputText = prompt("I am thinking of a color in the range \
-red, green, blue, orange, yellow brown, violet, gray, black, \
-white, purple, pink, golden, silver, cyan \n \
+    guessInputText = prompt("I am thinking of a color in the range \n\n\
+black, blue, brown, cyan, golden, gray, green, orange, pink, purple, \
+red, silver, violet, white, yellow \n\n\
 Enter your guess colors ^^: ");
     guessCount++;
     finisihed = check_guess(guessInputText, guessTarget); 
   }
-  alert();
 }
 
 function check_guess(InputText,Target){
   for (var i = colors.length - 1; i >= 0; i--) {
     if(colors[i] == InputText){
-      if(i == Target){
-        alert("you are right! \n" + "You guess " + guessCount + " times");
+      if     (InputText == colors[Target]){
+        myBody=document.getElementsByTagName("body")[0];
+        myBody.style.background=colors[Target];
+        setTimeout(function(){alert("Congratulation!, you are right! \n" + "You guess " + guessCount + " times \n" +
+              "You coud see the color in backgroud");},200); 
         return true;
       }
-      else if(i > Target){
-        alert("Your color is higher target");
+      else if(InputText > colors[Target]){
+        alert("Sorry, your guess is not correct!\n" + "Hint: Your color is alphabetically higher than mine");
         return false;
       }
-      else if(i < Target){
-        alert("Your color is lower target");        
+      else if(InputText < colors[Target]){
+        alert("Sorry, your guess is not correct!\n" + "Hint: Your color is alphabetically lower than mine");
         return false;
       }
     }
